6e79352b735cf944904b1e98868182d320fd105c
waleedfiaz663-cpu <waleedfiaz663@gmail.com>
2026-05-15 17:59:07 +0500

    Add files via upload

---
+++ factorial.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+	int num,i;
+	int fact=1;
+	printf("Enter a num = ");
+	scanf("%d",&num);
+	for(i=1;i<=num;i++)
+	{
+		fact=fact*i;
+	}
+		printf("Factorial of %d = %d\n",num,fact);
+}
