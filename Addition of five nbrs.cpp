7cbcd801665eb48875563722db33c7fe5f84c858
waleedfiaz663-cpu <waleedfiaz663@gmail.com>
2026-05-15 14:30:56 +0500

    Add files via upload

---
+++ Addition of five nbrs.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,d,e,addition;
+	printf("Enter First Number = ");
+	scanf("%d",&a);
+	printf("Enter Second Number = ");
+	scanf("%d",&b);
+	printf("Enter Third Number = ");
+	scanf("%d",&c);
+	printf("Enter Fourth Number = ");
+	scanf("%d",&d);
+	printf("Enter Fifth Number = ");
+	scanf("%d",&e);
+	addition=a+b+c+d+e;
+	printf("The addition of five nbrs are : %d+%d+%d+%d+%d=%d",a,b,c,d,e,addition);
+}
+	
